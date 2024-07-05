f7a370e907e280585649a6a452e556808b757dd2
DyslexicLemons <dyslexiclemonsjm@gmail.com>
2024-07-05 12:28:34 -0400

    editing text in httpRequests file

--- TestHttpRequests.py
+++ TestHttpRequests.py
@@ -5,9 +5,11 @@ from datetime import datetime
 
 
 headers = {
-    "Authorization" : 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE3MjAxMDMwNjcsInVzZXJfbmFtZSI6Impvd2FtYWpvIiwic2NvcGUiOlsicmVhZCJdLCJleHAiOjE3MjAxNDYyNjcsImF1dGhvcml0aWVzIjpbIlJPTEVfU1RVREVOVCIsIlJPTEVfQUNNX1VTRVIiLCJST0xFX01GQV9VU0VSIiwiUk9MRV9TVEFGRiJdLCJqdGkiOiJISU9OSGFtcS15WkxYd2pwTy1FQ091U3l2STAiLCJjbGllbnRfaWQiOiJwaWUifQ.DS8pmotT0TUB5EwZR-6L_jUAAR-FBZwtIqcUFhz588ofCvb7qT5u5qSTKWfh3Fwsa1dGwLX_5041sHYx7wD6lnSNPF8pjLUQMfJ33axBB-TM5A2JaQ7_xwJ9iJLjp1rWlgFsAJ5019mi13LcHhI2zU33BOo3VYSJQEALHQ3PyQ91sMxah5VR4dZd7PJrVdq4Z2od7t3uammyIJa1vCDgBkEUCufCISlTP4M0pCXJ0ok1qcN0gUn52fFUUglja-eO7I1y1zOxHODrki6sDWfs_U4xWNwfwt-ZGRESR1jXW3W7p4XQCyye3DFtzNu9oM7gTlSunhsmJCaGFcef00EIhw'
+
+    "Authorization" : 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE3MjAxODEwMTEsInVzZXJfbmFtZSI6Impvd2FtYWpvIiwic2NvcGUiOlsicmVhZCJdLCJleHAiOjE3MjAyMjQyMTEsImF1dGhvcml0aWVzIjpbIlJPTEVfU1RVREVOVCIsIlJPTEVfQUNNX1VTRVIiLCJST0xFX01GQV9VU0VSIiwiUk9MRV9TVEFGRiJdLCJqdGkiOiI1clFQR2xoZWJhN1U1Uk9TREVuWVJPUWt0MWciLCJjbGllbnRfaWQiOiJwaWUifQ.DiVzsv3KsirLKCzIaF9AxQQ8NAfsVYPoVYf2RgBrfMqp-80HnvAItMmUO2hwxFoNNVD5wJwrokl-ZJ6hJtSOLhpK4qtifcsf_zjV6aHBJ0fYxRUyvo2rTQWXFxnxYEccVkptn9IiaNmNtoHcGk7Pk_s_1Oj9QhYfP5xCzNyrAeyM4wQJz1ggGFKs-9gSYNENngHMKbZSgoQrRBUV_IaC9whAHciWBfyM9FOonYhVQtL59Tr8O3aMAfpQzclzx-UvuO1HXljpLPuHf5hQiB1gvW0EpCWJ9DbZjauSKRapCzjInyUE4ChfAthjXghzqV9Gh20r3rg1-XNZecjJ-Yq01Q'
+
 }
-now_iso = datetime.now().isoformat()
+now_iso = datetime.now().isoformat()            # Current date/time in iso format
 # auth = ('jowamajo', 'Taryn Lily Bowie.')      # Use to retrieve JWT
 
 """
@@ -15,12 +17,14 @@ Request all consultants schedules
 """
 BASE_URL = 'https://scfl.pie.iu.edu/Api/Shifts'
 params = {
+
     "page" : "0",
     "pageLimit" : "0",
     "minimal" : "true",
     "weekOf" : now_iso,
     "groupById" : "1",
     "formatById" : "1"
+
 }
 response = requests.get(BASE_URL,headers=headers,params=params)
 
@@ -29,7 +33,7 @@ print(response)
 content = response.content
 decoded_json = json.loads(content.decode('utf-8'))
 parsed_json = json.dumps(decoded_json, indent = 4)
-# print(parsed_json)
+print(parsed_json)
 # """
 
 # Request one consultant schedule
