233f52ac1b424fc511dafdc08a56a6469cfe03a4
DyslexicLemons <dyslexiclemonsjm@gmail.com>
2024-07-05 13:02:15 -0400

    add jwt dictionary

--- TestHttpRequests.py
+++ TestHttpRequests.py
@@ -1,16 +1,27 @@
 import requests                                 # Http request library for python
+
 import json
+
 import psycopg2                                 # PostgreSQL library for Python
+
 from datetime import datetime
 
+import socket
+
+now_iso = datetime.now().isoformat()            # Current date/time in iso format
+# auth = ('jowamajo', 'Taryn Lily Bowie.')      # Use to retrieve JWT
+jwt_tokens = {
+    
+    "IU-1BSVFY3" : "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE3MjAxODEwMTEsInVzZXJfbmFtZSI6Impvd2FtYWpvIiwic2NvcGUiOlsicmVhZCJdLCJleHAiOjE3MjAyMjQyMTEsImF1dGhvcml0aWVzIjpbIlJPTEVfU1RVREVOVCIsIlJPTEVfQUNNX1VTRVIiLCJST0xFX01GQV9VU0VSIiwiUk9MRV9TVEFGRiJdLCJqdGkiOiI1clFQR2xoZWJhN1U1Uk9TREVuWVJPUWt0MWciLCJjbGllbnRfaWQiOiJwaWUifQ.DiVzsv3KsirLKCzIaF9AxQQ8NAfsVYPoVYf2RgBrfMqp-80HnvAItMmUO2hwxFoNNVD5wJwrokl-ZJ6hJtSOLhpK4qtifcsf_zjV6aHBJ0fYxRUyvo2rTQWXFxnxYEccVkptn9IiaNmNtoHcGk7Pk_s_1Oj9QhYfP5xCzNyrAeyM4wQJz1ggGFKs-9gSYNENngHMKbZSgoQrRBUV_IaC9whAHciWBfyM9FOonYhVQtL59Tr8O3aMAfpQzclzx-UvuO1HXljpLPuHf5hQiB1gvW0EpCWJ9DbZjauSKRapCzjInyUE4ChfAthjXghzqV9Gh20r3rg1-XNZecjJ-Yq01Q"
+}
+hostname = socket.gethostname()
 
 headers = {
 
-    "Authorization" : 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE3MjAxODEwMTEsInVzZXJfbmFtZSI6Impvd2FtYWpvIiwic2NvcGUiOlsicmVhZCJdLCJleHAiOjE3MjAyMjQyMTEsImF1dGhvcml0aWVzIjpbIlJPTEVfU1RVREVOVCIsIlJPTEVfQUNNX1VTRVIiLCJST0xFX01GQV9VU0VSIiwiUk9MRV9TVEFGRiJdLCJqdGkiOiI1clFQR2xoZWJhN1U1Uk9TREVuWVJPUWt0MWciLCJjbGllbnRfaWQiOiJwaWUifQ.DiVzsv3KsirLKCzIaF9AxQQ8NAfsVYPoVYf2RgBrfMqp-80HnvAItMmUO2hwxFoNNVD5wJwrokl-ZJ6hJtSOLhpK4qtifcsf_zjV6aHBJ0fYxRUyvo2rTQWXFxnxYEccVkptn9IiaNmNtoHcGk7Pk_s_1Oj9QhYfP5xCzNyrAeyM4wQJz1ggGFKs-9gSYNENngHMKbZSgoQrRBUV_IaC9whAHciWBfyM9FOonYhVQtL59Tr8O3aMAfpQzclzx-UvuO1HXljpLPuHf5hQiB1gvW0EpCWJ9DbZjauSKRapCzjInyUE4ChfAthjXghzqV9Gh20r3rg1-XNZecjJ-Yq01Q'
+    "Authorization" : jwt_tokens[hostname]
 
 }
-now_iso = datetime.now().isoformat()            # Current date/time in iso format
-# auth = ('jowamajo', 'Taryn Lily Bowie.')      # Use to retrieve JWT
+
 
 """
 Request all consultants schedules
